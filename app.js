bd92167f384022870f7efd96dc5cc8c36cd5f21f
maskimkaran <turtleln7000@gmail.com>
2023-08-20 11:11:16 +0200

    app.js test

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-const express = require('express');
-const app = express();
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.end('Welcome');
+    }
+    if(req.url === '/bruh'){
+        res.end('BRUH PAGE');
+    }
+    res.end('Error 404');
+})
+
+//znaci da bi sve lepo poslao na git trebas prvo da uradis "git add ." a zatim "git commit -m "poruka"" i zatim "git push origin main"
